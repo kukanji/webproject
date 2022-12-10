133daedfa4ff29ba7dadc1307fca71c4c218fe7f
kukanji <111495470+kukanji@users.noreply.github.com>
2022-12-10 16:28:46 +0900

    ユーザー登録が重複した際のエラーメッセージの表示

--- webpost/views.py
+++ webpost/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 def signupview(request):
-    return render(request, 'signup.html', {'somedata': 100})
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        try:
+            user = User.objects.create_user(username_data, '', password_data)
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーは既に登録されています。'})
+    else:
+        return render(request, 'signup.html', {})
+    return render(request, 'signup.html', {})
 
