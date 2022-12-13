1dd2cedb964ac9daaf5ee0d8b8e50e6f322a56ef
kukanji <111495470+kukanji@users.noreply.github.com>
2022-12-13 17:06:50 +0900

    DetaiViewの作成

--- webpost/urls.py
+++ webpost/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import signupview, loginview, listview
+from .views import signupview, loginview, listview, detailview
 
 urlpatterns = [
     path('signup/',  signupview, name = 'signup'),
     path('login/', loginview, name = 'login'),
     path('list/', listview, name = 'list'),
-    
+    path('detail/<int:pk>/', detailview, name = 'detail')
 ]
