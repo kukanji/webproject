9e52180521368bc5b8c73d74946d0fe95c988a6f
kukanji <111495470+kukanji@users.noreply.github.com>
2022-12-13 16:38:40 +0900

    画像のアップロード機能の追加

--- webproject/urls.py
+++ webproject/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webpost.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # settings内のmedia_urlが合致した際にdocument_rootで定義した画像を呼び出す
